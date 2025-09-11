48c8bd52a4c26d3b513c9c026dfc37b89b5a370e
sostinewaliaula <83455882+sostinewaliaula@users.noreply.github.com>
2025-09-11 16:19:22 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,8 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5000,
-    // allowedHosts: [
-    //   'it.internal.services'
-    //   'it-assets.caavagroup.com'
-    // ]
+    allowedHosts: [
+      'it-assets.caavagroup.com'
+    ]
   }
 })
